26d35346ed21484e0ea6db663927b3efdc0afd4d
Mason Jeffers <masonjeffers@gmail.com>
2017-09-15 14:19:52 -0400

    Update pdutil.sh
    setsebool -P httpd_can_network_connect 1
    chcon -R -t httpd_sys_rw_content_t /usr/share/prizm/Samples/
    makes the php work, as opposed to not work

--- pdutil.sh
+++ pdutil.sh
@@ -101,6 +101,8 @@ function install_deb() {
 function install_rpm() {
 	# Update yum for fresh RPM installations
 	yum update &> /dev/null
+	setsebool -P httpd_can_network_connect 1
+	chcon -R -t httpd_sys_rw_content_t /usr/share/prizm/Samples/
 
 	echo "Installing PrizmDoc..."
 	if [[ ! "$EXCLUDE_SERVER" == true ]]; then
